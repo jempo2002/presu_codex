51b440bf26d573cd2f74635187be03fd61999ccc
Juan Esteban Montenegro Pelaez <129201398+jempo2002@users.noreply.github.com>
2025-07-23 13:10:27 -0500

    Add password confirmation field

--- blueprints/registro/static/registro.js
+++ blueprints/registro/static/registro.js
@@ -17,6 +17,11 @@ document.addEventListener("DOMContentLoaded", function() {
             elementId: "contraseñaUsuario",
             regex: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
             errorMessage: "Su contraseña debe tener almenos una letra mayuscula una minuscula y un numero, minimo 8 caracteres</br>"
+        },
+        {
+            elementId: "confirmarContraseñaUsuario",
+            regex: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+            errorMessage: "Confirme su contraseña correctamente</br>"
         }
     ];
 
@@ -32,6 +37,13 @@ document.addEventListener("DOMContentLoaded", function() {
                 isValid = false;
             }
         });
+
+        const pass = document.getElementById("contraseñaUsuario").value;
+        const confirm = document.getElementById("confirmarContraseñaUsuario").value;
+        if (pass !== confirm) {
+            padre.innerHTML += "Las contraseñas no coinciden</br>";
+            isValid = false;
+        }
 
         if (isValid) {
             enviar.submit();
