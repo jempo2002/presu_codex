c719e69d217feeaff4c22650228fa20da5b98b46
Juan Esteban Montenegro Pelaez <129201398+jempo2002@users.noreply.github.com>
2025-07-23 15:46:01 -0500

    Add email verification workflow

--- config.py
+++ config.py
@@ -9,6 +9,12 @@ class DevelopmentConfig(Configuracion):
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = ''
     MYSQL_DB = 'presucostosv2'
+    MAIL_SERVER = 'localhost'
+    MAIL_PORT = 25
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = False
 
 configuracion = {
     'development': DevelopmentConfig
